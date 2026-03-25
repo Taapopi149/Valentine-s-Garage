f67561a36d81c422a12a35643bf54cd808f68596
Taapopi <taapopin@telecom.na>
2026-03-25 17:07:48 +0200

    file structure

---
+++ app/src/main/java/com/example/valentinesgarage/Screens/Employee/EmployeeHomePage.kt
@@ -0,0 +1,4 @@
+package com.example.valentinesgarage.Screens.Employee
+
+class EmployeeHomePage {
+}
---
+++ app/src/main/java/com/example/valentinesgarage/Screens/Manager/ManagerHomePage.kt
@@ -0,0 +1,4 @@
+package com.example.valentinesgarage.Screens.Manager
+
+class ManagerHomePage {
+}
